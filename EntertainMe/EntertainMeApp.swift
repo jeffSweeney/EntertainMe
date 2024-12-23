d832e2a2595e8f3aaaa39b103597a95e0454aece
jeffSweeney <92553377+jeffSweeney@users.noreply.github.com>
2024-12-23 14:57:50 -0500

    Initial commit

---
+++ EntertainMe/EntertainMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  EntertainMeApp.swift
+//  EntertainMe
+//
+//  Created by Jeffrey Sweeney on 12/23/24.
+//
+
+import SwiftUI
+
+@main
+struct EntertainMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
