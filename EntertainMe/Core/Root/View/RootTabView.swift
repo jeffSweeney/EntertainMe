acf4788f32e1e012f09ff4489ff9047de06775a7
jeffSweeney <92553377+jeffSweeney@users.noreply.github.com>
2024-12-28 17:30:31 -0500

    Add new colorsets for the app

---
+++ EntertainMe/Core/Root/View/RootTabView.swift
@@ -0,0 +1,18 @@
+//
+//  RootTabView.swift
+//  EntertainMe
+//
+//  Created by Jeffrey Sweeney on 12/28/24.
+//
+
+import SwiftUI
+
+struct RootTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RootTabView()
+}
