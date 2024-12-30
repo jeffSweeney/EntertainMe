952f4452b22d84c7291a31b72599c0e3d0e03d3d
jeffSweeney <92553377+jeffSweeney@users.noreply.github.com>
2024-12-30 15:34:38 -0500

    Add button shadowing

--- EntertainMe/Components/Modifiers/EMButtonViewModifier.swift
+++ EntertainMe/Components/Modifiers/EMButtonViewModifier.swift
@@ -15,5 +15,6 @@ struct EMButtonViewModifier: ViewModifier {
             .foregroundStyle(.emButtonText)
             .fontWeight(.bold)
             .controlSize(.extraLarge)
+            .shadow(color: .emSecondary.opacity(0.5), radius: 8, x: 4, y: 4)
     }
 }
