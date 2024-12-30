88e05f9750eca55a54c39a42d38de37679ef0669
jeffSweeney <92553377+jeffSweeney@users.noreply.github.com>
2024-12-30 14:51:59 -0500

    Migrate the button view modifier

--- EntertainMe/Components/Modifiers/EMButtonViewModifier.swift
+++ EntertainMe/Components/Modifiers/EMButtonViewModifier.swift
@@ -5,4 +5,15 @@
 //  Created by Jeffrey Sweeney on 12/30/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct EMButtonViewModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .buttonStyle(.borderedProminent)
+            .tint(.emPrimary)
+            .foregroundStyle(.emButtonText)
+            .fontWeight(.bold)
+            .controlSize(.extraLarge)
+    }
+}
