9b50debc2dd5776117e7b3740add5429302d861e
jeffSweeney <92553377+jeffSweeney@users.noreply.github.com>
2024-12-30 14:53:47 -0500

    Implement new generic content presenter with corresponding usecase types

--- EntertainMe/Components/EMContentPresenter/EMContentPresenterProtocol.swift
+++ EntertainMe/Components/EMContentPresenter/EMContentPresenterProtocol.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+protocol EMContentPresenterProtocol {
+    var logo: String { get }
+    var content: String { get }
+}
+
+final class EMDadJokePresenter: EMContentPresenterProtocol {
+    let logo: String = "theatermasks.fill"
+    let content: String
+    
+    init(joke: String) {
+        content = joke
+    }
+}
+
+final class EMRandomFactPresenter: EMContentPresenterProtocol {
+    let logo: String = "lightbulb.fill"
+    let content: String
+    
+    init(fact: String) {
+        content = fact
+    }
+}
+
+final class EMErrorPresenter: EMContentPresenterProtocol {
+    let logo: String = "exclamationmark.octagon.fill"
+    let content: String
+    
+    init(error: String) {
+        content = error
+    }
+}
--- EntertainMe/Components/EMContentPresenter/EMContentPresenterView.swift
+++ EntertainMe/Components/EMContentPresenter/EMContentPresenterView.swift
@@ -7,13 +7,46 @@
 
 import SwiftUI
 
-struct EMContentPresenterView: View {
+struct EMContentPresenterView<T: EMContentPresenterProtocol>: View {
+    @Environment(\.dismiss) var dismiss
+    
+    let presenter: T
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 36) {
+            Image(systemName: presenter.logo)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 200)
+                .foregroundStyle(.emPrimary)
+            
+            Text(presenter.content)
+                .foregroundStyle(.emSecondary)
+                .font(.headline)
+                .padding(.horizontal, 24)
+                .multilineTextAlignment(.center)
+            
+            Button("More Entertainment!") {
+                dismiss()
+            }
+            .modifier(EMButtonViewModifier())
+            .padding(.vertical, 36)
+        }
     }
 }
 
-#Preview {
-    EMContentPresenterView()
+#Preview("Dad Joke") {
+    EMContentPresenterView(presenter: EMDadJokePresenter(joke: "What do you call a cow with no legs? A steer!"))
+}
+
+#Preview("Random Fact") {
+    EMContentPresenterView(presenter: EMRandomFactPresenter(fact: "Bacon is a type of meat that is not actually meat."))
+}
+
+#Preview("Error - Dad Joke") {
+    EMContentPresenterView(presenter: EMErrorPresenter(error: "Dad must be mowing the lawn. No jokes available at this time. Try again later."))
+}
+
+#Preview("Error - Random Fact") {
+    EMContentPresenterView(presenter: EMErrorPresenter(error: "Fact: We encountered an error trying to fetch a fact ... awkward. Try again later."))
 }
