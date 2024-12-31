653e01e116108e50932bfe18743b8e119e6a375c
jeffSweeney <92553377+jeffSweeney@users.noreply.github.com>
2024-12-31 11:50:02 -0500

    Build TriviaButtonView component

---
+++ EntertainMe/Core/Trivia/View/TriviaButtonView.swift
@@ -0,0 +1,52 @@
+//
+//  TriviaButtonView.swift
+//  EntertainMe
+//
+//  Created by Jeffrey Sweeney on 12/31/24.
+//
+
+import SwiftUI
+
+struct TriviaButtonView: View {
+    @State private var borderColor: Color = .emSecondary
+    @State private var hasBeenTapped: Bool = false
+    
+    let content: String
+    let correctAnswer: Bool
+    
+    var body: some View {
+        Button(action: {
+            withAnimation(.easeInOut(duration: 0.2)) {
+                borderColor = correctAnswer ? .green : .red
+                hasBeenTapped = true
+            }
+        }) {
+            ZStack {
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(Color.clear)
+                Text(content)
+                    .padding(6)
+                    .foregroundStyle(.emPrimary)
+                    .font(.headline)
+                    .minimumScaleFactor(0.5)
+            }
+            .frame(width: 300, height: 65)
+            .overlay {
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(borderColor, lineWidth: 3)
+            }
+            .scaleEffect(hasBeenTapped ? 0.95 : 1.0) // Shrinks slightly when tapped
+        }
+        .disabled(hasBeenTapped)
+    }
+}
+
+#Preview {
+    VStack(spacing: 16) {
+        TriviaButtonView(content: "Incorrect Answer", correctAnswer: false)
+        TriviaButtonView(content: "Correct Answer", correctAnswer: true)
+        TriviaButtonView(content: "Incorrect Answer", correctAnswer: false)
+        TriviaButtonView(content: "Incorrect Answer", correctAnswer: false)
+
+    }
+}
---
+++ EntertainMe/Core/Trivia/View/TriviaView.swift
@@ -0,0 +1,18 @@
+//
+//  TriviaView.swift
+//  EntertainMe
+//
+//  Created by Jeffrey Sweeney on 12/31/24.
+//
+
+import SwiftUI
+
+struct TriviaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TriviaView()
+}
